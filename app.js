a160e940c154f9764eaa7411111db7e9615b2f64
Artur Campos Miranda <artcamir12@gmail.com>
2022-01-31 18:39:53 -0300

    javascript working

--- app.js
+++ app.js
@@ -23,20 +23,40 @@ function add(){
     console.log(array);
 
     input.value = "";
-}
-
-function removeAll(){
-    array = [];
-
-    console.log(array);
 
     print();
 }
 
-function sort(){
-   Math.round Math.random();
+function removeAll(){
+    let confirm = window.confirm("Desja remover todos os itens?");
+        if (confirm){ 
+            array = [];
+
+            console.log(array);
+        
+            print();
+        }
+        else{
+        }
 }
 
 function print(){
+    let list = document.getElementById('list');
 
+    list.innerHTML = "";
+    array.forEach((item)=>{
+        let li = document.createElement("li");
+        li.innerText = item;
+        list.appendChild(li);
+      })
 }
+
+function sort(){
+    let np = array.length;
+    
+    let ns = Math.floor(Math.random() * np);
+
+    let p = document.getElementById("displaySorted");
+    p.innerText = "O sorteado(a) foi " + array[ns];
+}
+
